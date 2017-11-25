f7f722a32924642ebe29f4e6571468898ee515c7
Nathan Icart <nathan.icart@gmail.com>
2017-11-25 15:44:37 -0600

    added angular

---
+++ src/index.js
@@ -0,0 +1,29 @@
+angular.module('App', ['elasticsearch'])
+
+.service('client', function (esFactory) {
+  return esFactory({
+    host: 'localhost:9200',
+    log: 'trace'
+  });
+})
+
+.controller('MainCtrl', function($scope, client, esFactory) {
+  client.search({
+    index: 'twitter',
+    q: 'message:the',
+    size: 10
+  })
+  .then(resp => {
+    $scope.results = resp.hits.hits;
+    $scope.error = null;
+  })
+  .catch(err => {
+    $scope.results = null;
+    $scope.error = err;
+
+    if (err instanceof esFactory.errors.NoConnections) {
+      $scope.error = new Error('Unable to connect to elasticsearch. ' +
+        'Make sure that it is running and listening at http://localhost:9200');
+    }
+  });
+});
